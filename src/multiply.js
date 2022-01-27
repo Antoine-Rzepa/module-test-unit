c70e54e45044758a2932945f477f5fcc6092c9d3
Antoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2022-01-27 13:33:48 +0100

    function multiply + practice with TDD

---
+++ src/multiply.js
@@ -0,0 +1,11 @@
+export function multiply(a, b){
+    //GREEN
+    if(isNaN(a)){
+        return a + " is not a number" 
+    }else if (isNaN(b)){
+        return b + " is not a number"
+    }else{
+        //GREEN
+        return a * b
+    }
+}
---
+++ test/multiply.test.js
@@ -0,0 +1,16 @@
+
+import {multiply} from "../src/multiply";
+
+//RED
+test('multiply 3 x 5 to equal 15', () => {
+    expect(multiply(3, 5)).toBe(15)
+});
+
+//RED
+test('add test_first_param x 2, it should return test_first_param is not a number', () =>{
+    expect(multiply("test_first_param", 2)).toEqual("test_first_param is not a number");
+})
+ 
+test('add 2 x test_second_param, it should return test_second_param is not a number', () =>{
+    expect(multiply(2, "test_second_param")).toEqual("test_second_param is not a number");
+})
