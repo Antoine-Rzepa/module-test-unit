0ef8f7332f73c4d02c073f4fd0aeefce5379b75f
Antoine-Rzepa <antoine.rzepa@le-campus-numerique.fr>
2022-01-27 13:07:58 +0100

    first commit

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,10 @@
 // Implement the sum function here
 export function sum(a, b) {
+    if(isNaN(a)){
+        return a + " is not a number" 
+    }else if (isNaN(b)){
+        return b + " is not a number"
+    }else{
+        return a + b
+    }
 }
--- test/sum.test.js
+++ test/sum.test.js
@@ -4,3 +4,11 @@ import {sum} from "../src/sum";
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('add test_first_param + 2, it should return test_first_param is not a number', () =>{
+  expect(sum("test_first_param", 2)).toEqual("test_first_param is not a number");
+})
+
+test('add 2 + test_second_param, it should return test_second_param is not a number', () =>{
+  expect(sum(2, "test_second_param")).toEqual("test_second_param is not a number");
+})
